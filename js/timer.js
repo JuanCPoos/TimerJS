cca17b07bc3a4309bb4fed9d84d9204f4ebd62bb
JuanCPoos <juan.cpoos@gmail.com>
2022-10-24 23:49:09 -0300

    Se cargarón imagenes play/stop y sonido alarma.

---
+++ js/timer.js
@@ -0,0 +1,13 @@
+let inputs, clock, alarm, hours, minutes, seconds, repeater;
+
+window.addEventListener('load', () => {
+    inputs = Array.from(document.getElementsByClassName('number'));
+    clock = document.querySelector('.clock');
+    alarm = new Audio('sound/alarma.mp3');
+});
+
+function startTimer() { 
+    //Leer inputs
+    // actualizar vista
+    // Arrancar el timer
+}
