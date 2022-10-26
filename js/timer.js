ec34b28a379cbe2b41ce19578d51b78b7ca86fd4
JuanCPoos <juan.cpoos@gmail.com>
2022-10-25 21:47:27 -0300

    Funciones de inicio, seteo y contador, mas alarma.

--- js/timer.js
+++ js/timer.js
@@ -8,6 +8,63 @@ window.addEventListener('load', () => {
 
 function startTimer() { 
     //Leer inputs
-    // actualizar vista
+    parseTime();
+    // actualizar vista, al valor ingrtesado por usuario
+    setTimer();
     // Arrancar el timer
+    countdown();
+
+
 }
+
+function parseTime() { 
+    /* transformamos los valores de string ingresados por input
+    a valores Number*/
+    hours = Number(inputs[0].value);
+    minutes = Number(inputs[1].value);
+    seconds = Number(inputs[2].value);
+}
+
+function setTimer() {
+    /** actualiza la pantalla con hs-min-seg
+    y actualiza también el title de la pestaña*/
+    clock.innerHTML = `<p class="number">${hours > 9 ? hours : ('0' + hours)}</p><span>hs</span> 
+    <p class="number">${minutes > 9 ? minutes : ('0' + minutes)}</p><span>hs</span>
+    <p class="number">${seconds > 9 ? seconds : ('0' + seconds)}</p><span>hs</span>`;
+
+    document.title = `${hours > 9 ? hours : ('0' + hours)}:${minutes > 9 ? minutes : ('0' + minutes)}:${seconds > 9 ? seconds : ('0' + seconds)}`;
+}
+
+function countdown() {
+    // da inició al contador.
+    repeater = setInterval(runner, 1000)
+}
+ 
+function runner(){ 
+/** calcula el tiempo que falta para hacer sonar la alarma y llama 
+ * a la función setTimer() para actualizar pantalla y title del document-
+ * además hace sonar la alarma una vez cumplido el plazo ingresado.
+*/
+    if (seconds > 0) {
+        seconds--;    
+    } else {
+        if (minutes > 0) {
+            minutes--;
+        }else {
+            if (hours > 0) {
+                hours--;
+            } else {
+                alarm.play();
+            }
+        }            
+    }
+    setTimer();
+}
+
+function stopTimer() { 
+    // con reload de la pagina no sería necesario limíar el intervalo. 
+    clearInterval(repeater);
+    location.reload();
+}
+
+
